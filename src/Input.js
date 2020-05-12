225e6c498ddd7bae46811ea34b4749eeca241f93
Christopher Wattaul <chris.wattaul@gmail.com>
2020-05-12 16:24:46 +0200

    Add input.js

--- src/Input.js
+++ src/Input.js
@@ -1,27 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 // this comment tells babel to convert jsx to calls to a function called jsx instead of React.createElement
 /** @jsx jsx */
 import { css, jsx } from '@emotion/core';
-
+// import randomColor from './randomColor';
+// handleInput = function () {};
 export default function Input() {
   // const color = randomColor()
-  // const [color, setColor] = useState(randomColor());
+  const [color, setColor] = useState();
   return (
-    <input
-      type='text'
-      placeholder='get a less random color'
-      css={css`
-        width: auto;
-        height: auto;
-        left: 331px;
-        top: 425px;
-        background: grey;
-      `}
-      // onClick={function onClick() {
-      //   setColor(randomColor());
-      // }}
-    >
-      {/* click here {color} */}
-    </input>
+    <form>
+      <input
+        type='text'
+        name='this is'
+        placeholder='name your color'
+        css={css`
+          position: absolute;
+          width: 844px;
+          height: 64px;
+          left: 331px;
+          top: 120px;
+          background: ${color} !important;
+        `}
+        onChange={function onChange(event) {
+          setColor(event.target.value);
+        }}
+      ></input>
+    </form>
   );
 }
+// export default function Input() {
+//   return (
+//     <form onSubmit={this.handleSubmit}>
+//       <label>
+//         Color:
+//         <input
+//           type="text"
+//           value={this.state.value}
+//           onChange={this.handleChange}
+//         />{' '}
+//       </label>
+//       <input type="submit" value="Submit" />
+//     </form>
+//   );
+// }
